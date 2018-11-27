538ac3fe98416158b3c067c39a6d986d9ba88c4b
namor007 <roberto.namor@ufersa.edu.br>
2018-11-27 11:09:56 -0300

    new file:   Fortran.f

---
+++ Fortran.f
@@ -0,0 +1,9 @@
+       Program Create_Universe
+C      =================================================
+C        este programa é para o universo
+C      ================================================
+       Implicit None
+       Real*8 a !--------------------------------------> essa variavel mede a energia
+       a = 2.d0
+       write(*,*)'a = ', a
+       End 
